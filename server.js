82747bf64d426338ed9e7f7c2b816126f2c9f5c0
a-saika <saika1154ok@gmail.com>
2025-02-20 21:35:04 +0900

    #fix

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const https = require('http');
+const http = require('http');
 const fs = require('fs');
 const { Server } = require('socket.io');
 const cors = require('cors');
